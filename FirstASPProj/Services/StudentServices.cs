d6b9c414060f3d490c29a5d6e179945b29d3cb4e
aavash07 <aavash07@users.noreply.github.com>
2020-12-01 12:12:52 +0545

    sqlConnection

--- FirstASPProj/Services/StudentServices.cs
+++ FirstASPProj/Services/StudentServices.cs
@@ -1,6 +1,8 @@
 ﻿using FirstASPProj.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +13,50 @@ namespace FirstASPProj.Services
         public List<StudentModel> GetAllStudent()
         {
             List<StudentModel> students = new List<StudentModel>();
-            students.Add(new StudentModel { id = 1, name = "Aavash", address = "satdobato" });
+            /*students.Add(new StudentModel { id = 1, name = "Aavash", address = "satdobato" });
             students.Add(new StudentModel { id = 2, name = "Sabin", address = "Bhaktapur" });
             students.Add(new StudentModel { id = 3, name = "Bibke", address = "Jadibuti" });
+*/
+            var conStr = "Data Source=DESKTOP-KDHNFAA;initial catalog=Orchid;integrated security=True;MultipleActiveResultSets=True";
+            
+            /*var cmd = conn.CreateCommand();
+            cmd.CommandText = "Select * from student";*/
+            var sql = "Select * from student";
+
+            /*conn.Open();*/
+            /*SqlDataReader result=cmd.ExecuteReader();*/
+
+            using (var conn = new SqlConnection(conStr))
+            {
+                var adapter = new SqlDataAdapter(sql, conn);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+
+
+
+
+                /*while (result.Read())*/
+                foreach(DataRow row in ds.Tables[0].Rows)
+                {
+                    StudentModel std = new StudentModel
+                    {
+                        /*id = (int)result["Id"],
+                        name = result["Name"].ToString(),
+                        email = result["Email"].ToString(),
+                        address = result["Address"].ToString()*/
+                        id = (int)row["Id"],
+                        name = row["Name"].ToString(),
+                        email = row["Email"].ToString(),
+                        address = row["Address"].ToString()
+                    };
+                    students.Add(std);
+                }
+
+            }
+
+            /*conn.Close();
+            conn.Dispose();*/
+
 
             return students;
         }
